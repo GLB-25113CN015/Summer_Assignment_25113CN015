31769e987b86710a8deec8d048e69baa78ba1c04
Himanshu <himanshu2003109@gmail.com>
2026-06-28 16:53:36 +0530

    Day-3 Summer Assignment

---
+++ Day-3/Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        flag = 0;
+    } else {
+        for (i = 2; i < num; i++) {
+            if (num % i == 0) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if (flag == 1)
+        printf("%d is a Prime Number\n", num);
+    else
+        printf("%d is not a Prime Number\n", num);
+
+    return 0;
+}
